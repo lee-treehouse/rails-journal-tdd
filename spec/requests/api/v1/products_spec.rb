813aeeae8a895447fcc869f5cae5e0b4f91a0171
leentaylor <leentaylor@gmail.com>
2023-10-20 14:32:22 +1100

    from https://www.youtube.com/watch?v=a92OdqXZ1zs - add an rspec endpoint test (which will fail)

---
+++ spec/requests/api/v1/products_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+Rspec.describe "Products endpoints", request: true do 
+    describe "POST /api/v1/products" do
+        context "when all parameters are provided" do 
+            it "creates a product" do 
+                params = {
+                    product: {
+                        name: "Fancy sneakers",
+                        description: "Lorem ipsum dolor sit", 
+                        price: 5000
+                    }
+                }
+
+                expect do 
+                    post api_v1_products_url, params: params
+                end.to change(Product, :count).by(1) 
+            end
+        end
+    end
+end
