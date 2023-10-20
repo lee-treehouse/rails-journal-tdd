625d6789170ba06e214f9fa727b6bf1d823c840b
leentaylor <leentaylor@gmail.com>
2023-10-20 15:15:12 +1100

    specify params

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ProductsController < ApplicationController
-    def create
-        Product.create
-    end
+  def create
+    Product.create(product_params)
+  end
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
