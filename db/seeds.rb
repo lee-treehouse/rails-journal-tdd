ca33578afb46ffe6a24480c38a92637f6843c924
leentaylor <leentaylor@gmail.com>
2023-11-04 15:24:52 +1100

    create db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# seeding - https://ninjadevel.com/seeding-database-ruby-on-rails/
+Coupon.destroy_all
+
+Coupon.create!([{
+description:"All items 50% discount",
+kind:"ALL_ITEM",
+discount:0.5, 
+coupon_id:1
+},{
+    description:"All items 20% discount",
+    kind:"ALL_ITEMS",
+    discount:0.2, 
+    coupon_id:2
+    },{
+        description:"One item 50% discount",
+        kind:"ONE_ITEM",
+        discount:0.5, 
+        coupon_id:3
+        },{
+            description:"One item 20% discount",
+            kind:"ONE_ITEM",
+            discount:0.2, 
+            coupon_id:4
+            }])
